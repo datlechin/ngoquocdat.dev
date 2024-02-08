18b0675f7261c93a8d353ed05bd3e748472e31ab
Ngo Quoc Dat <datlechin@gmail.com>
2024-02-09 04:07:52 +0700

    New

---
+++ src/components/SocialItem.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+  href: string
+}>()
+</script>
+
+<template>
+  <a
+    :href="href"
+    target="_blank"
+    class="flex items-center gap-3 text-gray-300 hover:text-gray-50 transition-all"
+  >
+    <slot />
+    <span class="hidden sm:block">{{ title }}</span>
+  </a>
+</template>
---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Be Vietnam Pro"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config
