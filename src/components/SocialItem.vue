f484e77368cc134c6b09cf00e0a8d567d1a13c92
Ngo Quoc Dat <datlechin@gmail.com>
2024-02-24 15:02:21 +0700

    Improve accessbility

--- src/components/SocialItem.vue
+++ src/components/SocialItem.vue
@@ -10,6 +10,7 @@ defineProps<{
     :href="href"
     target="_blank"
     class="flex items-center gap-3 text-gray-300 hover:text-gray-50 transition-all"
+    :aria-label="title"
   >
     <slot />
     <span class="hidden sm:block">{{ title }}</span>
