675e61e4f0bbf90ce25afce57575b91dd6af0aaa
Ngo Quoc Dat <datlechin@gmail.com>
2024-05-21 23:45:58 +0700

    wip

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,7 @@ import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router'
 
 const routes: RouteRecordRaw[] = [
   { path: '/', component: () => import('./views/HomeView.vue') },
-  { path: '/cv', component: () => import('./views/ResumeView.vue') },
+  { path: '/cv', component: () => import('./views/CvView.vue') },
 ]
 
 const router = createRouter({
